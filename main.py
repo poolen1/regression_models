ce0d0b1f1257d11c36f776b974609054d11b7693
poolen108 <poolen108@gmail.com>
2021-05-01 16:11:43 -0700

    iterate rows for offer status value

--- main.py
+++ main.py
@@ -12,8 +12,26 @@ user_data = pd.read_json(user_data_file, lines=True)
 promo_data = pd.read_json(promo_data_file, lines=True)
 transaction_data = pd.read_json(transaction_data_file, lines=True)
 
-print(user_data.to_string())
-print(promo_data.to_string())
-print(transaction_data.to_string())
+offer_completed = pd.DataFrame();
+offer_received = pd.DataFrame();
+
+"""
+for i in range(len(transaction_data)):
+    # print(transaction_data.loc[i]['event'])
+    if transaction_data.loc[i]['event'] == "transaction":
+        print(transaction_data.loc[i])
+"""
+
+for i in range(len(transaction_data)):
+    if transaction_data.loc[i]["event"] == "offer completed":
+        offer_completed.append(transaction_data.loc[i-1])
+        offer_completed.append(transaction_data.loc[i])
+    elif transaction_data.loc[i]["event"] == "offer received":
+        offer_received.append(transaction_data.loc[i])
+
+print(offer_completed.to_string())
+# print(user_data.to_string())
+# print(promo_data.to_string())
+# print(transaction_data.to_string())
 
 # EOF
