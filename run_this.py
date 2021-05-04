4e361c492971942e98b0d735d90b2c976f660623
presley <57386908+presleyduggan@users.noreply.github.com>
2021-05-04 00:05:35 -0700

    get the results :fire:
    y'all got anymore of that data?

--- run_this.py
+++ run_this.py
@@ -1,1 +1,19 @@
 from models import Model
+
+# needs data to be added :) 
+def main():
+    features = 7
+    doML = Model(features)
+    # knn_results = doML.KNN(X, Y)
+    # LR_results = doML.lRegression(X, Y)
+    # Gradient_results = doML.descent(X, Y)
+    # NN_results = doML.doNeuralNetwork(X, Y)
+    # print(knn_results)
+    # print(LR_results)
+    # print(Gradient_results)
+    # print(NN_results)
+
+
+
+if __name__ == "__main__":
+    main()
