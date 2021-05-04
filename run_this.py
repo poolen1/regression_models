8a624eb1ea15b79eaba7313c5c9b404a031cfc47
presley <57386908+presleyduggan@users.noreply.github.com>
2021-05-04 00:45:50 -0700

    3 experiment runs - added quick read in for data

--- run_this.py
+++ run_this.py
@@ -4,14 +4,24 @@ from models import Model
 def main():
     features = 7
     doML = Model(features)
-    # knn_results = doML.KNN(X, Y)
-    # LR_results = doML.lRegression(X, Y)
-    # Gradient_results = doML.descent(X, Y)
-    # NN_results = doML.doNeuralNetwork(X, Y)
-    # print(knn_results)
-    # print(LR_results)
-    # print(Gradient_results)
-    # print(NN_results)
+
+    # X = pd.read_csv('X.csv')
+    # Y = pd.read_csv('Y.csv')
+    
+    # for i in range(0,3): # repeat each experiment 3 times
+        # print("Run # :"+str(i))
+        # print("Doing KNN")
+        # knn_results = doML.KNN(X, Y)
+        # print("KNN done -- Doing LR")
+        # LR_results = doML.lRegression(X, Y)
+        # print("LR done -- Doing Gradient")
+        # Gradient_results = doML.descent(X, Y)
+        # print("Gradient done -- Doing NN")
+        # NN_results = doML.doNeuralNetwork(X, Y)
+        # print("KNN results are: "+str(knn_results))
+        # print("Linear Regression results are: "+str(LR_results))
+        # print("Gradient results are: "+str(Gradient_results))
+        # print("NN results are: "+str(NN_results))
 
 
 
