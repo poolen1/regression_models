2dd7cf49ff02a9e8688885e6b9e1a7bf3ab61948
presley <57386908+presleyduggan@users.noreply.github.com>
2021-05-13 20:36:46 -0700

    model changes
    mini-batch, some other stuff

--- models.py
+++ models.py
@@ -1,47 +1,24 @@
+from sklearn import model_selection
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn import metrics
-from sklearn.model_selection import GridSearchCV, KFold, cross_val_score, cross_validate
+from sklearn.model_selection import GridSearchCV, KFold, cross_validate, learning_curve
 from sklearn.linear_model import LinearRegression, SGDRegressor
 from keras import models, layers, optimizers
 from keras.wrappers.scikit_learn import KerasClassifier
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 class Model:
     def __init__(self, n_features):
         self.features = n_features
         self.cv = KFold(n_splits=10, shuffle=True)
         self.cross_scoring = ['neg_root_mean_squared_error', 'r2']
-        # maybe add something here -> input data?
-    
-    def format_data(self, data):
-        print("this will format data")
-        # randomize
-        # split into x and y
-        # scale x 
-        # return 10-fold
-
-    #def adjusted_r2(self, y_true, y_pred, n, p): not using this because it's too late... let's just use r^2
-    # calculates adjusted r2
-    # y_true and y_pred from model
-    # n = number of observations in sample
-    # p = number of independent variables
-        #r2 = metrics.r2_score(y_true, y_pred)
-        #adjusted = 1-(1-r2)*((n-1)/(n-p-1))
-        #return adjusted
 
     def KNN(self, X, Y, total_neighbors = 7):
-        #cv = KFold(n_splits=10, shuffle=True)
-        #cross_scoring = ['neg_root_mean_squared_error', 'r2']
         model = KNeighborsRegressor(n_neighbors= total_neighbors)
         scores = cross_validate(model, X, Y, scoring=self.cross_scoring, cv=self.cv, n_jobs=-1)
-        #knn.fit(x_train, y_train)
-        #y_pred = knn.predict(x_test)
-        #rmse = metrics.mean_squared_error(y_test, y_pred, squared=False) # root mean squared error
-        #print("KNN RSME = " + str(rmse))
-        # do adjusted_r2
-        #r2 = 0 #placeholder
         return scores
 
     def find_best_N(self, x_train, y_train): # will output best N -> not sure if we want to use this
@@ -51,33 +28,86 @@ class Model:
         model.fit(x_train, y_train)
         return model.best_params_
 
-    def descent(self, X, Y):
-        #cv = KFold(n_splits=10, shuffle=True)
-        #cross_scoring = ['neg_root_mean_squared_error', 'r2']
-        model = SGDRegressor()
-        scores = cross_validate(model, X, Y, scoring=self.cross_scoring, cv=self.cv, n_jobs=-1)
-        #for i in range(0, fold):
-        #    model.partial_fit(x_train[i], y_train[i])
+    def descent(self, X, Y, folds, n_chunks):
+        gmodel = SGDRegressor(loss="huber",eta0=0.00000001)
+        scores = cross_validate(gmodel, X, Y, scoring=self.cross_scoring, cv=self.cv, n_jobs=-1)
 
-        #once done get values
-        #y_pred = model.predict(x_test)
-        #rmse = metrics.mean_squared_error(y_test, y_pred, squared=False) # root mean squared error
-        #print("Descent RSME = " + str(rmse))
-        # do adjusted_r2
-        #r2 = 0 #placeholder
+        # graphing
+        """ train_sizes, train_scores, test_scores = model_selection.learning_curve(estimator=gmodel, X=X, y=Y, cv=10)
+        train_scores_mean = np.mean(train_scores, axis=1)
+        train_scores_std = np.std(train_scores, axis=1)
+        test_scores_mean = np.mean(test_scores, axis=1)
+        test_scores_std = np.std(test_scores, axis=1)
+        
+        _, axes = plt.subplots(1, 3, figsize=(20, 5))
+        axes[0].set_title("graph")
+        axes[0].set_xlabel("Training examples")
+        axes[0].set_ylabel("Score")
+        axes[0].grid()
+        axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
+                            train_scores_mean + train_scores_std, alpha=0.1,
+                            color="r")
+        axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
+                            test_scores_mean + test_scores_std, alpha=0.1,
+                            color="g")
+        axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
+                    label="Training score")
+        axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
+                    label="Cross-validation score")
+        axes[0].legend(loc="best")
+        plt.show() """
+
+        # mini batch
+        """ x_folds = np.array_split(X, folds)
+        y_folds = np.array_split(Y, folds)
+        scores = {}
+        r2 = []
+        rmse = []
+        #print(X)
+
+        for i in range(0, folds):
+            # split into test and training --> current i value will be the test set
+            list_o_nums = []
+
+            for k in range(0, folds):
+                if k != i:
+                    list_o_nums.append(k)
+
+            y_test = y_folds[i]
+            x_test = x_folds[i]
+            for j in range(0, len(list_o_nums)):
+                if j == 0:
+                    x_train = x_folds[list_o_nums[j]]
+                    y_train = y_folds[list_o_nums[j]]
+                else:
+                    x_train = np.concatenate((x_train, x_folds[list_o_nums[j]]))
+                    y_train = np.concatenate((y_train, y_folds[list_o_nums[j]]))
+
+            x_chunk = np.array_split(x_train, n_chunks)
+            y_chunk = np.array_split(y_train, n_chunks)
+
+            for counter in range(0, n_chunks):
+                gmodel.partial_fit(x_chunk[counter], y_chunk[counter])
+
+            #once done get values
+
+            y_pred = gmodel.predict(x_test)
+            rmse.append(metrics.mean_squared_error(y_test, y_pred, squared=False)) # root mean squared error
+            r2.append(metrics.r2_score(y_test, y_pred))
+            #plt.plot(x_test, y_pred, "r-")
+            #plt.plot(x_test, y_test, "b.")
+            #plt.show()
+
+        scores["r2"] = r2
+        scores["rmse"] =rmse """
+
+        
+
         return scores
 
     def lRegression(self, X, Y):
-        #cv = KFold(n_splits=10, shuffle=True)
-        #cross_scoring = ['neg_root_mean_squared_error', 'r2']
         model = LinearRegression()
         scores = cross_validate(model, X, Y, scoring=self.cross_scoring, cv=self.cv, n_jobs=-1)
-        #model.fit(x_train,y_train)
-        #y_pred = model.predict(x_test)
-        #rmse = metrics.mean_squared_error(y_test, y_pred, squared=False) # root mean squared error
-        #print("Linear Regression RSME = " + str(rmse))
-        # do adjusted_r2
-        #r2 = 0 #placeholder
         return scores
 
     def create_Network(self):
@@ -88,28 +118,11 @@ class Model:
         network.compile(optimizer='adam', loss='mse', metrics=[tf.keras.metrics.RootMeanSquaredError(), "mae"])
         return network
         
-    """ def NeuralNetwork(self, x_train, y_train, x_test, y_test, n_shapes):
-        network = models.Sequential()
-        network.add(layers.Dense(24, activation='relu', input_shape=(n_shapes,)))
-        network.add(layers.Dense(24, activation='relu'))
-        network.add(layers.Dense(1))
-        network.compile(optimizer=optimizers.RMSprop(lr=0.01), loss='mse', metrics=['mae'])
-        output = network.fit(x_train,y_train,validation_data=(x_test,y_test),epochs=18,batch_size=20)
-        output_values = output.history
-        sme = output_values['loss']
-        validation_sme = output_values['val_loss']
-        # will need to take sqrt of these validation values ^
-        #calculate r2 
-        r2 = 0
-        return rmse """
-
     def doNeuralNetwork(self, X, Y):
         neural_network = KerasClassifier(build_fn = self.create_Network, 
-                                 epochs=15, 
+                                 epochs=20, 
                                  batch_size=500, 
                                  verbose=1)
-        #cv = KFold(n_splits=10, shuffle=True)
-        #results = cross_val_score(neural_network, X, Y, cv=self.cv)
         results = cross_validate(neural_network, X, Y, scoring=self.cross_scoring, cv=self.cv)
         return results
 
--- run_this.py
+++ run_this.py
@@ -1,21 +1,30 @@
 from models import Model
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-# needs data to be added :) 
 def main():
-    features = 8
+    features = 9
     doML = Model(features)
 
-    data_set = pd.read_csv('data_set.csv')
+    data_set = pd.read_csv('data_offer.csv')
     y_list = data_set.loc[:, 'amount']
     X_list = data_set.drop('amount', axis=1)
     Y = y_list.values
     X = np.array(X_list)
+    sum0 = data_set.loc[data_set['offer_type'] == 0, 'amount'].sum()
+    sum1 = data_set.loc[data_set['offer_type'] == 1, 'amount'].sum()
+    sum2 = data_set.loc[data_set['offer_type'] == 2, 'amount'].sum()
+    print(sum0, sum1, sum2)
 
-    #print(X)
+    #print(Y)
+    #print(np.cov(Y))
+    #print(np.amax(Y))
+
+    #print(X_list)
     #print(Y)
     #exit()
+    #138953 total data
     
     for i in range(0,1): # repeat each experiment 3 times
         print("Run # :"+str(i))
@@ -24,7 +33,7 @@ def main():
         print("KNN done -- Doing LR")
         LR_results = doML.lRegression(X, Y)
         print("LR done -- Doing Gradient")
-        Gradient_results = doML.descent(X, Y)
+        Gradient_results = doML.descent(X, Y, 10, 2600)
         print("Gradient done -- Doing NN")
         NN_results = doML.doNeuralNetwork(X, Y)
         print("KNN results are: "+str(knn_results))
