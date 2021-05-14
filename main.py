5b2adc438da53119dddeb7838bbfe9858cb76652
presley <57386908+presleyduggan@users.noreply.github.com>
2021-05-13 23:01:18 -0700

    done!
    all done :)

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-
 import pandas as pd
+import time
 import numpy as np
-import matplotlib.pyplot as plt
 
+
+start_time = time.time()
+
+print("Start Time: ", start_time)
 
 user_data_file = "./data/profile.json"
 promo_data_file = "./data/portfolio.json"
@@ -12,23 +15,95 @@ user_data = pd.read_json(user_data_file, lines=True)
 promo_data = pd.read_json(promo_data_file, lines=True)
 transaction_data = pd.read_json(transaction_data_file, lines=True)
 
-offer_completed = pd.DataFrame();
-offer_received = pd.DataFrame();
+transactions = pd.DataFrame()
+offer_completed = pd.DataFrame()
+offer_received = pd.DataFrame()
 
-offer_completed = transaction_data.loc[transaction_data['event'].shift(-1) == 'offer completed']
-offer_completed = offer_completed.loc[offer_completed['event'] == 'transaction']
-offer_completed = offer_completed.append(transaction_data.loc[transaction_data['event'] == 'offer completed'])
+# data_set = pd.DataFrame(columns=['amount', 'gender', 'user_age', 'user_income', 'transaction_time',
+#                                  'offer_type', 'offer_diff', 'offer_reward', 'percent_complete', 'channels'])
+data_set = pd.DataFrame()
 
-offer_completed = offer_completed.sort_index()
+# Get all 'transaction' type rows
+transactions = transaction_data.loc[transaction_data['event'] == 'transaction']
 
-offer_received = transaction_data.loc[transaction_data['event'] == 'offer received']
+# Get all completed offer events
+offer_completed = transaction_data.loc[transaction_data['event'] == 'offer completed']
+transactions = transactions.append(offer_completed)
+transactions = transactions.sort_index()
 
-offer_completed.to_csv(r'./data/offer_completed.csv')
-# offer_received.to_csv(r'./date/offer_received.csv')
-# print(offer_completed, len(offer_completed))
-# print(offer_received.to_string())
-# print(user_data.to_string())
-# print(promo_data.to_string())
-# print(transaction_data.to_string())
+data_set = transactions
+
+# User Data
+data_set['gender'] = data_set['person'].map(user_data.set_index('id')['gender'])
+data_set['user_age'] = data_set['person'].map(user_data.set_index('id')['age'])
+data_set['user_income'] = data_set['person'].map(user_data.set_index('id')['income'])
+
+# Normalize user data
+max_age = data_set['user_age'].max()
+# data_set['user_age'] = data_set['user_age']/max_age
+# data_set['user_age'] = data_set['user_age'].round(3)
+
+max_income = data_set['user_income'].max()
+# data_set['user_income'] = data_set['user_income']/max_income
+# data_set['user_income'] = data_set['user_income'].round(3)
+
+# Promo Data
+data_set['offer_id'] = [d.get('offer_id') for d in transactions.value]
+data_set['offer_id'] = data_set['offer_id'].shift(periods=-1)
+data_set['amount'] = [d.get('amount') for d in transactions.value]
+data_set = data_set[data_set.event != 'offer completed']
+
+data_set['offer_type'] = data_set['offer_id'].map(promo_data.set_index('id')['offer_type']).astype('category')
+data_set['offer_diff'] = data_set['offer_id'].map(promo_data.set_index('id')['difficulty'])
+data_set['offer_reward'] = data_set['offer_id'].map(promo_data.set_index('id')['reward'])
+
+offer_channels = data_set['offer_id'].map(promo_data.set_index('id')['channels']).apply(pd.Series)
+
+data_set['web'] = offer_channels[0].apply(lambda i: i if i == 0 else 1)
+data_set['email'] = offer_channels[1].apply(lambda i: i if i == 0 else 1)
+data_set['mobile'] = offer_channels[2].apply(lambda i: i if i == 0 else 1)
+data_set['social'] = offer_channels[3].apply(lambda i: i if i == 0 else 1)
+
+# Normalize promo data
+# max_diff = data_set['offer_diff'].max()
+# data_set['offer_diff'] = data_set['offer_diff']/max_diff
+# data_set['offer_diff'] = data_set['offer_diff'].round(3)
+
+# max_reward = data_set['offer_reward'].max()
+# data_set['offer_reward'] = data_set['offer_reward']/max_reward
+# data_set['offer_reward'] = data_set['offer_reward'].round(3)
+
+del data_set['person']
+# del data_set['offer_id']
+del data_set['value']
+del data_set['event']
+
+data_set = data_set[['amount', 'gender', 'user_income', 'offer_type', 'offer_diff',
+                     'offer_reward', 'web', 'mobile', 'social', 'offer_id']]
+
+data_set['gender'] = data_set['gender'].replace([np.nan, 'M', 'F', 'O'], [0, 1, 2, 3])
+data_set['offer_type'] = data_set['offer_type'].replace([np.nan, 'bogo', 'discount', 'informational'], [0, 1, 2, 3])
+data_set = data_set.fillna(0)
+
+# data_set.reset_index(drop=True, inplace=True)
+
+data_set.to_csv('newdata_set.csv')
+
+"""
+y_list = data_set.loc[:, 'amount']
+X_list = data_set.drop('amount', axis=1)
+# X and y, training and test data
+y = y_list.values
+X = np.array(X_list)
+"""
+
+# print(data_set.to_string())
+# print(y)
+# print(X)
+# print("Length: ", len(data_set))
+
+end_time = time.time() - start_time
+
+print("Runtime: ", end_time)
 
 # EOF
--- models.py
+++ models.py
@@ -12,16 +12,17 @@ import matplotlib.pyplot as plt
 
 class Model:
     def __init__(self, n_features):
-        self.features = n_features
-        self.cv = KFold(n_splits=10, shuffle=True)
-        self.cross_scoring = ['neg_root_mean_squared_error', 'r2']
+        self.features = n_features # feature count
+        self.cv = KFold(n_splits=10, shuffle=False)  # shuffle = false because data is shuffled in run_this.py
+        self.cross_scoring = ['neg_root_mean_squared_error', 'r2'] # return r2 and RSME (negative)
 
     def KNN(self, X, Y, total_neighbors = 7):
-        model = KNeighborsRegressor(n_neighbors= total_neighbors)
-        scores = cross_validate(model, X, Y, scoring=self.cross_scoring, cv=self.cv, n_jobs=-1)
-        return scores
+        model = KNeighborsRegressor(n_neighbors= total_neighbors) # model
+        scores = cross_validate(model, X, Y, scoring=self.cross_scoring, cv=self.cv, n_jobs=-1) # rmse and r2
+        prediction = model_selection.cross_val_predict(model, X, Y, cv=self.cv, n_jobs=-1) # prediction values
+        return scores, prediction
 
-    def find_best_N(self, x_train, y_train): # will output best N -> not sure if we want to use this
+    def find_best_N(self, x_train, y_train): # will output best N 
         params = {'n_neighbors':[2,3,4,5,6,7,8,9]}
         knn = KNeighborsRegressor()
         model = GridSearchCV(knn, params)
@@ -29,45 +30,24 @@ class Model:
         return model.best_params_
 
     def descent(self, X, Y, folds, n_chunks):
-        gmodel = SGDRegressor(loss="huber",eta0=0.00000001)
-        scores = cross_validate(gmodel, X, Y, scoring=self.cross_scoring, cv=self.cv, n_jobs=-1)
+        gmodel = SGDRegressor(loss="huber",eta0=0.00000001) # model -- this loss rate gave us the best accuracy, huber gave us results as well
 
-        # graphing
-        """ train_sizes, train_scores, test_scores = model_selection.learning_curve(estimator=gmodel, X=X, y=Y, cv=10)
-        train_scores_mean = np.mean(train_scores, axis=1)
-        train_scores_std = np.std(train_scores, axis=1)
-        test_scores_mean = np.mean(test_scores, axis=1)
-        test_scores_std = np.std(test_scores, axis=1)
-        
-        _, axes = plt.subplots(1, 3, figsize=(20, 5))
-        axes[0].set_title("graph")
-        axes[0].set_xlabel("Training examples")
-        axes[0].set_ylabel("Score")
-        axes[0].grid()
-        axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
-                            train_scores_mean + train_scores_std, alpha=0.1,
-                            color="r")
-        axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
-                            test_scores_mean + test_scores_std, alpha=0.1,
-                            color="g")
-        axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
-                    label="Training score")
-        axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
-                    label="Cross-validation score")
-        axes[0].legend(loc="best")
-        plt.show() """
+        # stochastic model -- was used previously
+        """ scores = cross_validate(gmodel, X, Y, scoring=self.cross_scoring, cv=self.cv, n_jobs=-1)
+        prediction = model_selection.cross_val_predict(gmodel, X, Y, cv=self.cv, n_jobs=-1) """
 
         # mini batch
-        """ x_folds = np.array_split(X, folds)
+        x_folds = np.array_split(X, folds)
         y_folds = np.array_split(Y, folds)
         scores = {}
         r2 = []
         rmse = []
+        predicted_values = []
         #print(X)
 
         for i in range(0, folds):
             # split into test and training --> current i value will be the test set
-            list_o_nums = []
+            list_o_nums = [] # this is used to combine the training set into one np array (it is currently an array of 9 arrays)
 
             for k in range(0, folds):
                 if k != i:
@@ -75,6 +55,8 @@ class Model:
 
             y_test = y_folds[i]
             x_test = x_folds[i]
+
+            # combine test data
             for j in range(0, len(list_o_nums)):
                 if j == 0:
                     x_train = x_folds[list_o_nums[j]]
@@ -83,9 +65,11 @@ class Model:
                     x_train = np.concatenate((x_train, x_folds[list_o_nums[j]]))
                     y_train = np.concatenate((y_train, y_folds[list_o_nums[j]]))
 
-            x_chunk = np.array_split(x_train, n_chunks)
+            # split training data into n_chunks for mini-batch
+            x_chunk = np.array_split(x_train, n_chunks) 
             y_chunk = np.array_split(y_train, n_chunks)
 
+            # mini batch
             for counter in range(0, n_chunks):
                 gmodel.partial_fit(x_chunk[counter], y_chunk[counter])
 
@@ -94,28 +78,29 @@ class Model:
             y_pred = gmodel.predict(x_test)
             rmse.append(metrics.mean_squared_error(y_test, y_pred, squared=False)) # root mean squared error
             r2.append(metrics.r2_score(y_test, y_pred))
+            predicted_values.append(y_pred)
             #plt.plot(x_test, y_pred, "r-")
             #plt.plot(x_test, y_test, "b.")
             #plt.show()
 
-        scores["r2"] = r2
-        scores["rmse"] =rmse """
-
-        
-
-        return scores
+        flat_list = [item for sublist in predicted_values for item in sublist] # predictions is list of lists --> turn into one list
+        prediction = np.array(flat_list) # np array
+        scores["r2"] = r2 # save r2 in scores
+        scores["rmse"] =rmse # save rmse ins cores
+        return scores, prediction
 
     def lRegression(self, X, Y):
         model = LinearRegression()
         scores = cross_validate(model, X, Y, scoring=self.cross_scoring, cv=self.cv, n_jobs=-1)
-        return scores
+        prediction = model_selection.cross_val_predict(model, X, Y, cv=self.cv, n_jobs=-1)
+        return scores, prediction
 
-    def create_Network(self):
+    def create_Network(self): # creates network for cross_validate and cross_val_predict
         network = models.Sequential()
         network.add(layers.Dense(128, activation='relu', input_shape=(self.features,)))
         network.add(layers.Dense(64, activation='relu'))
         network.add(layers.Dense(1, activation='linear'))
-        network.compile(optimizer='adam', loss='mse', metrics=[tf.keras.metrics.RootMeanSquaredError(), "mae"])
+        network.compile(optimizer='adam', loss='mse', metrics=[tf.keras.metrics.RootMeanSquaredError(), "mae"]) 
         return network
         
     def doNeuralNetwork(self, X, Y):
@@ -124,7 +109,8 @@ class Model:
                                  batch_size=500, 
                                  verbose=1)
         results = cross_validate(neural_network, X, Y, scoring=self.cross_scoring, cv=self.cv)
-        return results
+        prediction = model_selection.cross_val_predict(neural_network, X, Y, cv=self.cv, n_jobs=-1)
+        return results, prediction
 
 
 
